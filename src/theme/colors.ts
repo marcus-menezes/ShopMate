a892cd93b6eceb0391336489e51c95151aea2e3a
Marcus Menezes <marcusmenezes2009@gmail.com>
2023-09-06 01:50:27 -0300

    feat(colors): create colors constant

---
+++ src/theme/colors.ts
@@ -0,0 +1,11 @@
+export const Colors = {
+    Primary: "#214252",
+    Secondary: "#3D9999",
+    Black: "#000",
+    White: "#FFF",
+    Red: "#F70000",
+    Gray: "#8f8f8f",
+    Success: "#80c883",
+};
+
+export default Colors;
